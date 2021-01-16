f06433481dabf5dd6eb2b01174bd7e4f495d9fc4
RalphHernandez <76414197+RalphHernandez@users.noreply.github.com>
2021-01-16 22:04:58 +0800

    Create Function
    started workingon the Create Function

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?
+    $plant_name = $_POST['plantName'];
+    $date_potted = $_POST['datePotted'];
+    $plant_status = $_POST['pstatus'];
+    $recommendation = $_POST['recommendation'];
+
+    $conn = new mysqli_connect('localhost', 'root', '', 'phr');
+    if($conn -> connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("INSERT INTO plant (plant_name, date_potted, plant_status, recommendation)
+        values(?, ?, ?, ?)");
+        $stmt -> bind_param("ssss", $plant_name, $date_potted, $plant_status, $recommendation);
+        $stmt -> execute();
+        echo "Plant successfully added!";
+        $stmt -> close();
+        $conn -> close();
+    }
+?>
