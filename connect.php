336ef1b39197d7d3c423137c6492d1f1e829520c
RalphHernandez <76414197+RalphHernandez@users.noreply.github.com>
2021-01-20 10:05:48 +0800

    Fixed PHP
    made interface more responsive

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
-<?
+<?php
     $plant_name = $_POST['plantName'];
     $date_potted = $_POST['datePotted'];
     $plant_status = $_POST['pstatus'];
     $recommendation = $_POST['recommendation'];
 
-    $conn = new mysqli_connect('localhost', 'root', '', 'phr');
+    $conn = new mysqli('localhost', 'root', '', 'phr');
     if($conn -> connect_error){
         die('Connection Failed : '.$conn->connect_error);
     }else{
